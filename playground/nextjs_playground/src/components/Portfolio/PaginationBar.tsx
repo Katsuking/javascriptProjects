58e7de859c3c6c882f224b8fbf520d4f2e4fc24f
Katsuking <legendsoccerforte5@gmail.com>
2023-10-22 16:35:46 +0900

    pagination の実装

--- playground/nextjs_playground/src/components/Portfolio/PaginationBar.tsx
+++ playground/nextjs_playground/src/components/Portfolio/PaginationBar.tsx
@@ -1,6 +1,53 @@
+import Link from "next/link";
+
 interface PaginationBarProps {
   currentPage: number; // to show a current page and make it unclickable.
   totalPages: number;
 }
 
-export default function PaginationBar() {}
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+}: PaginationBarProps) {
+  const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+  const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
+
+  const numberedPageItems: JSX.Element[] = [];
+
+  for (let page = minPage; page <= maxPage; page++) {
+    numberedPageItems.push(
+      <Link
+        href={"?page=" + page}
+        key={page}
+        // pointer-events-none: disable click
+        className={`join-item btn ${
+          currentPage === page ? "btn-active pointer-events-none" : ""
+        }`}
+      >
+        {page}
+      </Link>,
+    );
+  }
+
+  return (
+    <>
+      {/* 画面が小さい時と大きい時で表示を変える */}
+      <div className="join hidden sm:block">{numberedPageItems}</div>
+      <div className="join block sm:hidden">
+        {currentPage > 1 && (
+          <Link href={"?page=" + (currentPage - 1)} className="join-item btn">
+            previous
+          </Link>
+        )}
+        <button className="join-item btn pointer-events-none">
+          Page {currentPage}
+        </button>
+        {currentPage < totalPages && (
+          <Link href={"?page=" + (currentPage + 1)} className="join-item btn">
+            next
+          </Link>
+        )}
+      </div>
+    </>
+  );
+}

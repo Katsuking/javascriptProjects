1335678f1c9c1c47f71feb1ac393c6e846c4a22e
Katsuking <legendsoccerforte5@gmail.com>
2024-02-02 17:23:09 +0900

    delete row

--- playground/nextauth/actions/login.ts
+++ playground/nextauth/actions/login.ts
@@ -14,7 +14,6 @@ import * as z from 'zod'
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   // console.log(values)
-  4
   // client側のvalidationは簡単にbypassできてしまうので,server側でもしっかりvalidationはいる
   const validatedFields = LoginSchema.safeParse(values)
 

2686411b6d60d1d4bd0ae55d202872936d5c2a59
Katsuking <legendsoccerforte5@gmail.com>
2024-02-05 18:41:54 +0900

    learn about jwtwebtoken

---
+++ playground/nodejs_restapi_playground/jsonWebToken/README.md
@@ -0,0 +1,4 @@
+### JWT for node.js
+
+node.js でよく使われるっぽい jsonwebtoken を使って、
+トークンの作成や検証を確認する
---
+++ playground/nodejs_restapi_playground/jsonWebToken/generateJWT.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const sleep = require("sleep");
+
+// payload
+const jwtPayload = {
+  email: "test@example.com",
+  name: "test jwt",
+};
+
+// secret
+const jwtSecret = "this_is_secret";
+
+// 使用されるアルゴリズム等
+const jwtOptions = {
+  algorithm: "HS256",
+  // expiresIn: "3m",
+  expiresIn: "3s",
+};
+
+// トークンの作成
+const token = jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+console.log("token: ", token);
+
+sleep.sleep(5); // 検証のために、有効期間を調整
+
+// トークンの検証
+jwt.verify(token, jwtSecret, (err, decoded) => {
+  if (err) {
+    console.log("Error: ", err.message);
+  } else {
+    console.log("token verified!!");
+    console.log("token:", decoded);
+  }
+});
